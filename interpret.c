c6c78f3e612b012e17bb555ecbe1af4fc4321716
Patricia charlotte BENEDETTO <110346262+cleobuline@users.noreply.github.com>
2025-04-10 17:03:14 +0200

    Update interpret.c

--- interpret.c
+++ interpret.c
@@ -43,63 +43,7 @@ void *env_interpret_thread(void *arg) {
     }
     return NULL;
 }
- /* OLD VERSION 
-void interpret(char *input, Stack *stack, Env *env) {
-    if (!env) {
-        send_to_channel("DEBUG: env is NULL");
-        return;
-    }
-    env->error_flag = 0;
-    env->compile_error = 0;
-    char *saveptr;
-    char *token = strtok_r(input, " \t\n", &saveptr);
-    while (token && !env->error_flag && !env->compile_error) {
-        if (strcmp(token, "(") == 0) {
-            char *end = strstr(saveptr, ")");
-            if (end) saveptr = end + 1;
-            else saveptr = NULL;
-            token = strtok_r(NULL, " \t\n", &saveptr);
-            continue;
-        }
-        compileToken(token, &saveptr, env);
-        if (!saveptr) break;
-        token = strtok_r(NULL, " \t\n", &saveptr);
-    }
 
-   
-    // Si on est en mode compilation et qu’il reste des structures de contrôle non terminées
-    if (env->compiling && env->control_stack_top > 0) {
-        set_error(env,"Incomplete definition: unmatched control structures");
-        env->compile_error = 1;
-        env->compiling = 0;
-        env->control_stack_top = 0;
-
-        // Libérer les ressources de currentWord pour éviter les fuites
-        if (env->currentWord.name) {
-            free(env->currentWord.name);
-            env->currentWord.name = NULL;
-        }
-        for (int j = 0; j < env->currentWord.string_count; j++) {
-            if (env->currentWord.strings[j]) {
-                free(env->currentWord.strings[j]);
-                env->currentWord.strings[j] = NULL;
-            }
-        }
-        if (env->currentWord.code) {
-            free(env->currentWord.code);
-            env->currentWord.code = NULL;
-        }
-        if (env->currentWord.strings) {
-            free(env->currentWord.strings);
-            env->currentWord.strings = NULL;
-        }
-        env->currentWord.code_length = 0;
-        env->currentWord.code_capacity = 0;
-        env->currentWord.string_count = 0;
-        env->currentWord.string_capacity = 0;
-    }
-}
-    */ 
     
 // new version to check for aborted defintion 
  
